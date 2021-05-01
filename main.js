6187a4144389014c6a3d3bf0b7be4059ebe74a1b
Nadia <nadia-2501@yandex.ru>
2021-05-01 17:54:05 +0300

    test

---
+++ main.js
@@ -0,0 +1,15 @@
+$ (function(){
+    $('.menu-open').click(function(){
+        $('.menu').toggleClass('show-menu')
+    })
+})
+$ (function(){
+    $('.menu-open').click(function(){
+        $('.promo__main').toggleClass('hide-title')
+    })
+})
+$ (function(){
+    $('.menu-open').click(function(){
+        $('.promo').toggleClass('backgr')
+    })
+})
